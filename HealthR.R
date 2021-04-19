db338fd8fa45b994986bf18a569fbf390baf3861
arjunblaze7 <62808375+arjunblaze7@users.noreply.github.com>
2021-04-19 14:11:35 -0500

    Update HealthR.R

--- HealthR.R
+++ HealthR.R
@@ -38,7 +38,6 @@ p <- plot_grid(a,b,c,d,e,f)
 title <- ggdraw() + draw_label(" Correlations ", fontface='bold')
 plot_grid(title, p, ncol=1, rel_heights=c(0.1, 1))
 
-![Dashboard 1](https://user-images.githubusercontent.com/62808375/115290283-f95f6e00-a118-11eb-9335-4bc0b14c873c.png)
 
 #Regression model
 n_train <- round(0.8 * nrow(Data))
